1168808bfa140fc4b46f5f9c52962609a5ee19d2
cyy <1162925744@qq.com>
2018-04-12 22:53:42 +0800

    10:53

--- db/migrate/20180412073818_add_avg_amount_to_product.rb
+++ db/migrate/20180412073818_add_avg_amount_to_product.rb
@@ -1,5 +1,5 @@
 class AddAvgAmountToProduct < ActiveRecord::Migration[5.0]
   def change
-  	add_column :products, :avg_amount, 
+  	add_column :products, :avg_amount, :decimal, precision: 8, scale: 2, default: 1
   end
 end
