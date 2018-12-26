cde827c4d0a3a912b9d1c32786966697df1c4af8
chen1125he <1162925744@qq.com>
2018-12-26 21:56:59 +0800

    config gix

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Jx3
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.eager_load_paths += %W(#{Rails.root}/lib)
+
+    config.time_zone = 'Beijing'
+    config.i18n.available_locales = [:en, :'zh-CN']
+    config.i18n.default_locale = :'zh-CN'
   end
 end
 
