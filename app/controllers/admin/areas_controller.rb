df1a10b63d264fd96098c60a3dced1d3b684a0aa
chen1125he <1162925744@qq.com>
2019-02-02 14:48:58 +0800

    area manage

---
+++ app/controllers/admin/areas_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class Admin::AreasController < Admin::BaseController
+  before_action :load_area, only: [:edit, :update, :destroy]
+
+  def index
+    q = Area.ransack(name_cont: params[:q])
+    @areas = q.result.order(created_at: :asc).page(params[:page])
+  end
+
+  def new
+    @area = Area.new
+  end
+
+  def create
+    @area = Area.new(area_params)
+    if @area.save
+      render_turbolinks_reload
+    else
+      render
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @area.update(area_params)
+      render_turbolinks_reload
+    else
+      render
+    end
+  end
+
+  def destroy
+    @area.soft_delete!
+
+    render_turbolinks_reload
+  end
+
+  private
+
+  def load_area
+    @area = Area.find(params[:id])
+  end
+
+  def area_params
+    params.require(:area).permit(:name)
+  end
+end
